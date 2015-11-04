dce1028db28a423153f281cb52f0b1e69bf91853
syan <family.syan@gmail.com>
2015-11-03 17:51:35 -0800

    init project

---
+++ SearchEntity/src/main/java/org/SearchEntity/App.java
@@ -0,0 +1,13 @@
+package org.SearchEntity;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
