ef5afafb57e700a5d4e7a75d143a2de8c60d81af
syan <family.syan@gmail.com>
2016-01-19 12:18:48 -0800

    interface fix

--- SearchService/src/main/java/com/sohungry/search/model/DistanceUnit.java
+++ SearchService/src/main/java/com/sohungry/search/model/DistanceUnit.java
@@ -1,13 +1,17 @@
 package com.sohungry.search.model;
 
 import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+import org.codehaus.jackson.annotate.JsonProperty;
 import org.codehaus.jackson.map.annotate.JsonSerialize;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonSerialize(include=JsonSerialize.Inclusion.NON_NULL)
 public enum DistanceUnit {
 	
+	@JsonProperty("mi")
 	MI,
+	
+	@JsonProperty("km")
 	KM
 
 }
--- SearchService/src/main/java/com/sohungry/search/model/SortBy.java
+++ SearchService/src/main/java/com/sohungry/search/model/SortBy.java
@@ -1,14 +1,20 @@
 package com.sohungry.search.model;
 
 import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+import org.codehaus.jackson.annotate.JsonProperty;
 import org.codehaus.jackson.map.annotate.JsonSerialize;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonSerialize(include=JsonSerialize.Inclusion.NON_NULL)
 public enum SortBy {
 	
+	@JsonProperty("hotness")
 	HOTNESS,
+	
+	@JsonProperty("distance")
 	DISTANCE,
+	
+	@JsonProperty("relevance")
 	RELEVANCE
 
 }
